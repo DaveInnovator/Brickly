ee4abd54b2abb89f07873ab66199432555fcbefa
DaveInnovator <daveola043@gmail.com>
2025-09-23 21:05:38 +0100

    blabla

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "images.unsplash.com",
-      "images.pexels.com",
-      "cdn.pixabay.com",
-      "images.pexels.com"
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
     ],
   },
 };
